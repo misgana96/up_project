b00c0e60e52efdab38725b46483667a2b93a6855
Neelu Bhargava <neelubhargava02@gmail.com>
2020-08-11 22:50:00 +0530

    code setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { MainComponent } from './auth/main/main.component';
+import { EmploymentComponent } from './auth/employment/employment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    SigninComponent,
+    MainComponent,
+    EmploymentComponent
   ],
   imports: [
     BrowserModule,
