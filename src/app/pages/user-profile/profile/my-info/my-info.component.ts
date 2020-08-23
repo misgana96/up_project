349b27a823a219906fe105ea8da2e067bcd65128
prince659 <sharmaprince659@gmail.com>
2020-08-23 19:35:27 +0530

    Added upload/preview functionality on image upload

--- src/app/pages/user-profile/profile/my-info/my-info.component.ts
+++ src/app/pages/user-profile/profile/my-info/my-info.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyInfoComponent implements OnInit {
 
+  public imageUrl: string | ArrayBuffer = '../../../assets/images/user_img.png'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  uploadFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.imageUrl = event.target.result;
+      }
+    }
   }
 
 }
