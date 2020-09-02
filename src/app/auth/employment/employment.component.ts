ceea13582df479b9269ef7213aa70715cd3d9e69
Aughtek <mahesh@aughtek.com>
2020-09-02 19:08:33 +0545

    package

--- src/app/auth/employment/employment.component.ts
+++ src/app/auth/employment/employment.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import $ from "jquery";
+declare var $: any;
 @Component({
   selector: 'app-employment',
   templateUrl: './employment.component.html',
   styleUrls: ['./employment.component.scss']
 })
 export class EmploymentComponent implements OnInit {
-  
+
   isEmployment: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(): void{
-    $(document).ready(function() {
-      $('.filebtn').on('click', function() {
+  ngAfterViewInit(): void {
+    $(document).ready(function () {
+      $('.filebtn').on('click', function () {
         $('.file').trigger('click');
       });
-    
-      $('.file').on('change', function() {
-        var fileName = $(this)[0].files[0].name;    
+
+      $('.file').on('change', function () {
+        var fileName = $(this)[0].files[0].name;
         $('#file-name').val(fileName);
       });
     })
   }
 
-  goSecondStep(){
+  goSecondStep() {
     this.isEmployment = !this.isEmployment
   }
 
