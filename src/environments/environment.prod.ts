173fa2ab3d82166ad596a1df9f3d9ed0b4ccc402
Aughtek <mahesh@aughtek.com>
2020-09-03 00:21:07 +0545

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyByNvO5YzjDSQwR1bTBozAEglKlXH-4FDs",
+    authDomain: "thajob-uti.firebaseapp.com",
+    databaseURL: "https://thajob-uti.firebaseio.com",
+    projectId: "thajob-uti",
+    storageBucket: "thajob-uti.appspot.com",
+    messagingSenderId: "10835062558",
+    appId: "1:10835062558:web:62321d99c353ee630c230f",
+    measurementId: "G-PZSV2QN6ZH"
+  }
 };
